f1344e58c52b9b4bf478cb3b9c8b7eecbc59bcbf
Da Y Mun <danamun@airbears2-10-142-35-238.AirBears2.1918.Berkeley.EDU>
2015-10-26 15:17:29 -0700

    Finished Hangman App.

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class HangmanViewController: UIViewController {
 
+    @IBOutlet weak var flowerImage: UIImageView!
+    @IBOutlet weak var knownStringTextView: UITextField!
     @IBOutlet weak var alphabetBtn: UIButton!
     @IBOutlet weak var guessBtn: UIButton!
+    let alphaList = [26:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H", 8:"I", 9:"J", 10:"K",
+        11:"L", 12:"M", 13:"N", 14:"O", 15:"P", 16:"Q", 17:"R", 18:"S", 19:"T", 20:"U", 21:"V",
+        22:"W", 23:"X", 24:"Y", 25:"Z"]
+    var currentFlowerNum = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        knownStringTextView.text = HangmanAPI?.knownString
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,11 +34,40 @@ class HangmanViewController: UIViewController {
     }
     
     @IBAction func alphabetClicked(sender: UIButton) {
-        sender.backgroundColor = UIColor.magentaColor()
+        sender.backgroundColor = UIColor.yellowColor() // UIColor.magentaColor()
         sender.enabled = false
-        var tag = sender.tag
-        print(tag)
+        let tag = sender.tag
+        let letter = alphaList[tag]
         
+        let guessedCorrect = HangmanAPI?.guessLetter(letter!)
+        if (guessedCorrect == true) {
+            knownStringTextView.text = HangmanAPI?.knownString
+            if (HangmanAPI?.knownString == HangmanAPI?.answer) {
+                flowerImage.image = UIImage(named: "winner")
+                knownStringTextView.text = "\"" + (HangmanAPI?.answer)! + "\" Click New Game!"
+                
+            }
+        } else {
+            currentFlowerNum = currentFlowerNum + 1
+            flowerImage.image = UIImage(named: "flower"+String(currentFlowerNum))
+            if (currentFlowerNum == 7) {
+                flowerImage.image = UIImage(named: "loser")
+                knownStringTextView.text = "Answer was: " + (HangmanAPI?.answer)! + " Click New Game!"
+            }
+        }
+    }
+    
+    @IBAction func newGameClicked(sender: AnyObject) {
+        HangmanAPI?.start()
+        flowerImage.image = UIImage(named: "flower0")
+        currentFlowerNum = 0
+        knownStringTextView.text = HangmanAPI?.knownString
+
+        for index in 0...26 {
+            let lettBtn = self.view.viewWithTag(index) as? UIButton
+            lettBtn?.enabled = true
+            lettBtn?.backgroundColor = UIColor.lightGrayColor()
+        }
     }
 }
 
